8cab85bf7ab7ff37374c1c5bec3c162798d8ccee
Chris Cosnowski <chris.cosnowski@gmail.com>
2018-11-11 03:12:51 -0500

    Second Commit

---
+++ WPF_DataViewer/DAL/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_DataViewer
+{
+    public interface IDataService
+    {
+        List<Product> ReadAll();
+        void WriteAll(List<Product> products);
+    }
+}
---
+++ WPF_DataViewer/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms_DataViewer
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Number { get; set; }
+        public double Price { get; set; }
+        public string DateReleased { get; set; }
+        public string Media { get; set; }
+        public double UnitsSold { get; set; }
+        public string Manufacturer { get; set; }
+        public string ImageFileName { get; set; }
+
+        public string ProductNameAndNumber()
+        {
+            return ProductName + (Number != null ? " " + Number : "");
+        }
+    }
+}
